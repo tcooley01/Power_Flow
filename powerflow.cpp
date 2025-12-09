da60355945eb1984ff930839ef2d48c81dadad57
Skyler Creedon Reid - 409610 <skyler@darwin-fe1.lanl.gov>
2025-12-09 11:16:42 -0700

    includes

---
+++ powerflow.cpp
@@ -0,0 +1,12 @@
+#include <cmath.h>
+#include <vector.h>
+#include <complex.h>
+
+/*
+structure notes
+read in Y matrix, S in, V in, type vector, tolerance
+calculate G, B matrices based on Y
+in while loop, calculate jacobian
+solve for mag and angle mismatches until tolerance in range
+store results in vectors
+*/
