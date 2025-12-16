91224bd3dce7eea036912733ff9cdea1eb1755b3
Taran William Cooley - 403053 <tcooley@darwin-fe3.lanl.gov>
2025-12-16 11:35:47 -0700

    jacobian update

--- jacobian.cpp
+++ jacobian.cpp
@@ -2,7 +2,26 @@
 #include <vector>
 using namespace std;
 
-vector<float> calculate_jacobian(){
+vector<vector<float>> calculate_jacobian(int N, vector<vector<float>> G, vector<vector<float>> B, vector<float> D, vector<float>){
+	vector<vector<float>> jacobian;
+	
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			if (i == j){
+				//exclude kth entry	
+				vector<int> include;
+					for (int k = 0; k < N; k++){
+						if (k != i){
+							include.push_back(k);
+						}
+					} 
+				
+				 
+			}else{
+
+			}
+		}
+	}
 
 
 
